32681d850ae375a1b4afd4a5290ebf9ad95802e7
qazws143 <3063941837@qq.com>
2025-07-15 21:40:45 +0800

    modified: "更新贪心算法"

--- Code/LeetCode/贪心算法/买卖股票的最佳时期.cpp
+++ Code/LeetCode/贪心算法/买卖股票的最佳时期.cpp
@@ -10,25 +10,23 @@ public:
         if (prices.empty())
             return 0;             // 处理空数组的情况
         int MinStock = prices[0]; // 记录最小股票值
-        int CurStock = 0;         // 记录当前股票值
         int CurProfit = 0;        // 记录最大利润
         int MaxProfit = 0;        // 上一利润值
         for (int i = 1; i < prices.size(); i++)
         {
-            CurStock = prices[i];
-            if (CurStock < MinStock) // 当前值小于假设的最小股票值
+            if (prices[i] < MinStock) // 当前值小于假设的最小股票值
             {
-                MinStock = CurStock;
+                MinStock = prices[i];
             }
             else
             {
-                CurProfit = CurStock - MinStock; // 计算当前利润
-                if (CurProfit > PreProfit)       // 如果当前利润大于上一次利润
+                CurProfit = prices[i] - MinStock; // 计算当前利润
+                if (CurProfit > MaxProfit)        // 如果当前利润大于上一次利润
                 {
-                    PreProfit = CurProfit;
+                    MaxProfit = CurProfit;
                 }
             }
         }
-        return PreProfit;
+        return MaxProfit;
     }
 };
