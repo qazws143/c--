f7e8f5a9b65e81508610ebf461ac536f61463145
qazws143 <3063941837@qq.com>
2025-07-17 20:12:50 +0800

    动态规划

--- Code/LeetCode/动态规划/打家劫舍.cpp
+++ Code/LeetCode/动态规划/打家劫舍.cpp
@@ -9,15 +9,17 @@ public:
     {
         if (nums.size() == 0)
             return 0;
+        if (nums.size() == 1)
+            return nums[0];
 
         int size = nums.size();
-        vector<int> dp(size + 1, 0);
-        dp[0] = 0;       // 表示房子没东西可偷
-        dp[1] = nums[0]; // 表示只有一个房子可偷
-        for (int i = 2; i <= nums.size(); i++)
+        vector<int> dp(size, 0);
+        dp[0] = nums[0];               // 只有一个房间时只能偷一个
+        dp[1] = max(nums[0], nums[1]); // 有两个房间时,哪个多偷哪个
+        for (int i = 2; i < nums.size(); i++)
         {
-            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2]); // 比较偷当前房子和不偷当前房子的最大值
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]); // 比较偷当前房子和不偷当前房子的最大值
         }
-        return dp[size];
+        return dp[size - 1];
     }
 };
